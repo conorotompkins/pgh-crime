f8abe76818283cdc447f4e17519a088de9d093d2
conorotompkins <conorotompkins@gmail.com>
2017-01-26 21:22:26 -0500

    updated analysis and changed graph

--- pgh_crime_analysis.R
+++ pgh_crime_analysis.R
@@ -45,13 +45,36 @@ df <- df %>%
 glimpse(df)
 
 
-df %>% 
-  filter(incident_year != 2017) %>% 
-  group_by(incident_date, incident_month) %>% 
+df %>%
+  group_by(incident_zone) %>% 
   count() %>% 
-  ggplot(aes(incident_date, n, color = incident_month, fill = incident_month)) +
+  arrange(-n)
+
+
+df %>% 
+  filter(incident_zone %in% c(1:6)) %>% 
+  group_by(incident_date, incident_zone) %>% 
+  count() %>% 
+  ggplot(aes(incident_date, n, color = incident_zone, fill = incident_zone)) +
+  geom_point(alpha = .02) +
   geom_smooth() +
   #facet_wrap(~incident_month, ncol = 1) +
   scale_fill_viridis(discrete = TRUE) +
+  scale_color_viridis(discrete = TRUE) +
+  coord_cartesian(ylim = c(0, 75))
+ggsave("pgh_crime_date_zone_line_plot.png", width = 16, height = 9)
+
+df %>%
+  group_by(incident_neighborhood) %>% 
+  count() %>% 
+  arrange(-n)
+
+df %>% 
+  filter(incident_zone %in% c(1:6)) %>% 
+  group_by(incident_zone, incident_neighborhood, incident_date) %>% 
+  count() %>% 
+  ggplot(aes(incident_date, n, color = incident_neighborhood, fill = incident_neighborhood)) +
+  geom_line() +
+  facet_wrap(~incident_zone, ncol = 1) +
+  scale_fill_viridis(discrete = TRUE) +
   scale_color_viridis(discrete = TRUE)
-ggsave("pgh_crime_date_month_line_plot.png", width = 16, height = 9)
