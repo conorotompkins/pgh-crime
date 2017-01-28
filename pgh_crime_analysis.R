986f766093631119ebe625ff53fdf52c5e05c574
conorotompkins <conorotompkins@gmail.com>
2017-01-28 12:09:45 -0500

    Changed column names Added narrative comments Added map plot

--- pgh_crime_analysis.R
+++ pgh_crime_analysis.R
@@ -5,92 +5,137 @@ source("https://raw.githubusercontent.com/conorotompkins/AdjGSAA/master/graphs/t
 library(tidyverse)
 library(lubridate)
 library(viridis)
+library(ggmap)
 
 theme_set(theme_nhh())
 
-data <- read_csv("archive-police-blotter.csv")
-problems(data)
+data <- read_csv("archive-police-blotter.csv") #Read tge data into R
+problems(data) #There are 6 rows that could not be processed
 
 df <- data
 
-colnames(df) <- tolower(colnames(df))
+colnames(df) <- tolower(colnames(df)) #Change all the column names to lower case
 
+#Rename the column names so they are easier to work with
 df <- df %>% 
-  rename(incident_date_time = incidenttime,
-         incident_location = incidentlocation,
+  rename(date_time = incidenttime,
+         location = incidentlocation,
          cleared_flag = clearedflag,
-         incident_neighborhood = incidentneighborhood,
-         incident_zone = incidentzone,
-         hierarchy_description = hierarchydesc,
-         incident_tract = incidenttract)
+         neighborhood = incidentneighborhood,
+         zone = incidentzone,
+         description = hierarchydesc,
+         tract = incidenttract)
+
 
 df <- df %>%
-  mutate(incident_date_time = mdy_hm(incident_date_time),
-         incident_date = ymd(substr(incident_date_time, 1, 10)),
-         incident_year = year(incident_date),
-         incident_month = month(incident_date, label = TRUE),
-         incident_wday = wday(incident_date, label = TRUE),
-         incident_mday = mday(incident_date)) %>% 
-  select(incident_date,
-         incident_year,
-         incident_month,
-         incident_wday,
-         incident_mday,
-         incident_location,
-         incident_neighborhood,
-         incident_zone,
-         hierarchy_description,
+  mutate(date_time = mdy_hm(date_time), #Create dates and times with Lubridate
+         date = ymd(substr(date_time, 1, 10)),
+         year = year(date),
+         month = month(date, label = TRUE),
+         wday = wday(date, label = TRUE),
+         mday = mday(date)) %>% 
+  select(date, #select the columnns we want
+         year,
+         month,
+         wday,
+         mday,
+         location,
+         neighborhood,
+         zone,
+         description,
          cleared_flag,
          x,
          y)
-glimpse(df)
 
+glimpse(df) #View the data
+
+#Which zone has the most arrests?
 df %>%
-  group_by(incident_zone) %>% 
+  group_by(zone) %>% 
   count() %>% 
   arrange(-n)
 
 
+#How has the number of arrests changed over time?
 df %>% 
-  filter(incident_zone %in% c(1:6)) %>% 
-  group_by(incident_date, incident_zone) %>% 
+  filter(zone %in% c(1:6)) %>% 
+  group_by(zone, date) %>% 
   count() %>% 
-  ggplot(aes(incident_date, n, color = incident_zone, fill = incident_zone)) +
-  geom_point(alpha = .02) +
+  ggplot(aes(date, n, color = zone, fill = zone)) +
+  #geom_point(alpha = .02) +
   geom_smooth() +
-  #facet_wrap(~incident_month, ncol = 1) +
+  #facet_wrap(~month, ncol = 1) +
   scale_fill_viridis(discrete = TRUE) +
   scale_color_viridis(discrete = TRUE) +
   coord_cartesian(ylim = c(0, 75))
 ggsave("pgh_crime_date_zone_line_plot.png", width = 16, height = 9)
 
+#Looks like there is a data or reporting problem in Zone 6
+
+#Which neighborhood has the most arrests?
 df %>%
-  group_by(incident_neighborhood) %>% 
+  group_by(neighborhood) %>% 
   count() %>% 
   arrange(-n)
 
+
 df %>% 
-  filter(incident_zone %in% c(1:6)) %>% 
-  group_by(incident_zone, incident_neighborhood, incident_date) %>% 
+  filter(zone == c(1:6)) %>% 
+  group_by(zone, neighborhood, date) %>% 
   count() %>% 
-  ggplot(aes(incident_date, n, color = incident_neighborhood, fill = incident_neighborhood)) +
+  arrange(zone, neighborhood, date) %>% 
+  mutate(cumsum = cumsum(n)) %>% 
+  ggplot(aes(date, cumsum, color = neighborhood, fill = neighborhood)) +
   geom_line() +
-  facet_wrap(~incident_zone, ncol = 1) +
+  facet_wrap(~zone, ncol = 1) +
   scale_fill_viridis(discrete = TRUE) +
   scale_color_viridis(discrete = TRUE)
 
-descriptions <- df %>%
+#Which arrest descriptions are the most common?
+df %>%
   #filter(incident_zone == 2) %>% 
-  group_by(hierarchy_description) %>% 
+  group_by(description) %>% 
   count() %>% 
   arrange(-n)
+#Many NA values
 
+#Have the number of marijuana-related arrests changed over time?
+#Create a dataframe
 pot_arrests <- df %>%
-  filter(grepl("MARIJUANA", hierarchy_description) == TRUE) %>%
-  group_by(incident_date) %>% 
+  filter(grepl("MARIJUANA", description) == TRUE) %>%
+  group_by(date) %>% 
   count() 
-str(pot_arrests)
 
-ggplot(data = pot_arrests, aes(x = incident_date, y = n)) +
+#Plot the data
+ggplot(data = pot_arrests, aes(x = date, y = n)) +
   geom_smooth()
+
+#Let's look at how the data looks on a map
+#First, create the map
+city_map <-  qmap("North Oakland, Pittsburgh, PA", 
+                  zoom = 12,
+                  maptype = "toner", 
+                  source = "stamen")
+
+#View the map to make sure it looks right
+city_map
+
+#Filter out data that is not in one of the six police zones
+df_map <- df %>% 
+  filter(zone %in% c(1:6))
+
+#Put the data on the map
+city_map +
+  stat_density_2d(data = df_map, #Using a 2d contour
+                  aes(x, #longitude
+                      y, #latitude
+                      fill = ..level.., #Use the count of arrests as the fill
+                      alpha = .5), #Use alpha so you can see the map under the data
+                  geom = "polygon") + #We want the contour in a polygon
+  #facet_wrap(~wday, nrow = 2) +
+  scale_fill_viridis() +
+  guides(alpha = FALSE,
+         fill = guide_colorbar("Count of Arrests")) +
+  theme(legend.position = "bottom",
+        legend.direction = "horizontal")
 
