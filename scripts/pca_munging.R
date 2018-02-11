6de082234c548b06572e8840b07c6b3f4c67aee0
Conor Tompkins <conorotompkins@gmail.com>
2018-02-11 20:09:31 +0000

    worked on PCA munging script

--- scripts/pca_munging.R
+++ scripts/pca_munging.R
@@ -1,14 +1,32 @@
 source("scripts/load_data_new.R")
 
 df %>% 
-  filter(date >= "2016-01-01") %>% 
-  count(neighborhood, sort = TRUE) %>% 
-  replace_na(list(neighborhood = "Not recorded")) -> df_top_nbh
+  filter(date >= "2016-01-01", !is.na(neighborhood)) -> df
 
+#consider filtering out "Not recorded"
 df %>%
-  filter(date >= "2016-01-01") %>% 
   count(description, sort = TRUE) %>% 
-  replace_na(list(description = "Not recorded")) -> df_top_crimes
+  replace_na(list(description = "Not recorded")) %>%
+  filter(n >= 500) -> df_top_crimes
+df_top_crimes
 
+description_list <- df_top_crimes$description
 
-                  
+df %>% 
+  semi_join(df_top_crimes) %>% 
+  count(neighborhood,description) %>% 
+  complete(neighborhood, description = description_list) %>% 
+  replace_na(list(n = 0)) %>% 
+  arrange(neighborhood, desc(n)) -> df
+
+df %>% 
+  ungroup() %>% 
+  group_by(neighborhood) %>% 
+  mutate(neighborhood_total = sum(n)) %>% 
+  ungroup() %>% 
+  mutate(crime_percentage = n / neighborhood_total) %>% 
+  select(-n, -neighborhood_total) -> df
+
+df %>% 
+  spread(key = description, value = crime_percentage) -> df
+df
